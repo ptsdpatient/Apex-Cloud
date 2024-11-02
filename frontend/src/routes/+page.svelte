7e49033c99f039e868646b5d774a9ab4df22aa30
Tanishq Dhote <119649887+ptsdpatient@users.noreply.github.com>
2024-11-02 12:32:47 +0530

    password visible and invisible icon implementation

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 
 let register=false
+let visible=false
 
 </script>
 
@@ -14,15 +15,18 @@ let register=false
 
         {#if register}
             <div class="bg-gray-200 w-10/12 text-xl flex flex-col justify-around gap-4 px-5 py-5 shadow-xl rounded-xl ">
-                <div class="text-center text-4xl my-3">Register </div>
+                <div class="text-center text-4xl my-2">Register </div>
             
                 <input class="w-11/12 shadow-sm mx-auto px-3 py-2 rounded-lg focus:outline-none bg-white" placeholder="username">        
                 <input class="w-11/12 shadow-sm mx-auto px-3 py-2 rounded-lg focus:outline-none bg-white" placeholder="email" type="email">        
-                <input class=" w-11/12 shadow-sm mx-auto px-3 py-2 rounded-lg focus:outline-none bg-white" placeholder="password" type="password">
+                <div class="relative flex flex-row items-center">
+                    <input class=" w-11/12 shadow-sm mx-auto px-3 py-2 rounded-lg focus:outline-none bg-white" placeholder="password" type={visible?"text":"password"}>
+                    <button class="absolute right-0 mr-4 pr-2 mx-auto" on:click={visible=!visible}><img alt="" src={visible?"/open.png":"/close.png"} ></button>
+                </div>
                 <button class="bg-blue-500 rounded-lg hover:shadow-lg w-2/5 py-2 text-xl px-5 text-white  mx-auto mt-2 transition-transform transform hover:scale-105 active:scale-95 focus:outline-none  ">Register</button>
                 
                 
-            <div class="mx-auto text-center my-1 w-full gap-1">
+            <div class="mx-auto text-center w-full gap-1">
                     <div class="text-wrap text-sm mx-auto">or Register with</div>
                     <button
                     class="w-2/3 mt-2 mb-3 mx-auto flex items-center justify-center border px-2 py-2.5 text-sm font-medium text-gray-700  rounded-lg shadow transition-transform transform hover:scale-105 active:scale-95 focus:outline-none bg-white">
@@ -42,12 +46,15 @@ let register=false
         
             <input class="w-11/12 shadow-sm mx-auto px-3 py-2 rounded-lg focus:outline-none bg-white" placeholder="username">
        
-            <input class=" w-11/12 shadow-sm mx-auto px-3 py-2 rounded-lg focus:outline-none bg-white" placeholder="password" type="password">
+            <div class="relative flex flex-row items-center">
+                <input class=" w-11/12 shadow-sm mx-auto px-3 py-2 rounded-lg focus:outline-none bg-white" placeholder="password" type={visible?"text":"password"}>
+                <button class="absolute right-0 mr-4 pr-2 mx-auto" on:click={visible=!visible}><img alt="" src={visible?"/open.png":"/close.png"} ></button>
+            </div>
 
             <button  class="bg-blue-500 rounded-lg hover:shadow-lg w-1/3 py-2 text-xl px-5 text-white  mx-auto mt-2 transition-transform transform hover:scale-105 active:scale-95 focus:outline-none  ">Login</button>
             
             
-        <div class="mx-auto text-center my-1 w-full gap-1">
+        <div class="mx-auto text-center w-full gap-1">
                 <div class="text-wrap text-sm mx-auto">or Sign in with</div>
                 <button
                 class="w-2/3 mt-2 mb-3 mx-auto flex items-center justify-center border px-2 py-2.5 text-sm font-medium text-gray-700  rounded-lg shadow transition-transform transform hover:scale-105 active:scale-95 focus:outline-none bg-white">
