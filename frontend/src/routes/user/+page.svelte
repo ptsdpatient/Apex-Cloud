ddf4487f7ffd3daafd50c167c58893d7e4160d5e
Tanishq Dhote <119649887+ptsdpatient@users.noreply.github.com>
2024-11-03 12:08:22 +0530

    Update +page.svelte

--- frontend/src/routes/user/+page.svelte
+++ frontend/src/routes/user/+page.svelte
@@ -5,7 +5,8 @@
     let full_name='Tanishq Dhote',email='tanishqbakka1@gmail.com'
     let token
     let url='http://localhost:2000/api'
-    let panelButtons=["Dashboard","Shared","Notifications","Bookmarks","Bin","Subscriptions","Help"]        
+    let panelButtons=["Dashboard","Shared","Notifications","Bookmarks","Bin","Subscriptions","Help"]   
+    let currentDirectory=["Home","Folder 1","Folder 2","Folder 3","Folder 4","Folder 5"]     
     let currentPanel=0
     function logout(){
         localStorage.removeItem("apex_cloud")
@@ -61,7 +62,7 @@
         </div>
         <div class="flex flex-grow gap-2 justify-around p-2 flex-row bg-gray-100">
             <div class="w-1/4 px-3 py-5 flex flex-col items-left align-left gap-2  h-full rounded-lg overflow-y-auto">
-                    <button class="text-lg rounded-lg py-2 my-1  transition-all bg-gray-500 hover:bg-blue-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Upload File</button>
+                    <button class="text-lg rounded-lg py-2 my-1  transition-all bg-gray-500 hover:bg-blue-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Upload Files</button>
               
                     {#each panelButtons as button,index}
                         <button on:click={currentPanel=index} class="{currentPanel==index?"bg-blue-200":""} focus:bg-blue-200 flex pl-2 py-2 flex-row items-center transform transition-all duration-100 focus:outline-none  hover:scale-105 outline-none focus:scale-105 hover:bg-white rounded-xl">
@@ -72,15 +73,21 @@
             </div>
             <div class="w-3/4 flex flex-col bg-white rounded-lg">
                 <div class="flex flex-row w-full text-xl justify-between py-3 px-2 gap-3 shadow-sm">
-                    <div class="flex flex-row gap-4">
-                        <button class="px-4 text-xl rounded-lg py-1 mr-5 hover:bg-gray-100 text-black transition-all ease-in-out duration-100">←</button>
-                        
-                    </div>
-                    <div>
-                        <button class="px-4 mr-4 text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-red-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Delete</button>
-              
-                    </div>
-              
+                        <div class="flex flex-row">
+                            <button class="px-4 text-xl rounded-lg py-1 mr-2 hover:bg-gray-100 text-black transition-all ease-in-out duration-100">←</button>
+                            <div class="flex flex-row text-xl">
+                                {#each currentDirectory as folder}
+                                    <div class="items-center align-center flex flex-row">
+                                        
+                                        <button class="transform hover:bg-opacity-50 hover:bg-gray-100 hover:scale-105 px-2 py-1 transition-all duration-100 ease-in-out rounded-xl">{folder}</button>
+                                        <div>
+                                            /
+                                        </div>
+                                    </div>   
+                                {/each}
+                            </div>
+                        </div>
+                        <button class="px-4 mr-4 text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-red-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Delete Folder</button>
                 </div>
 
             </div>
