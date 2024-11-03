7d30130415a812dfe173175198cf3e8c82615722
Tanishq Dhote <119649887+ptsdpatient@users.noreply.github.com>
2024-11-03 11:48:39 +0530

    Update +page.svelte

--- frontend/src/routes/user/+page.svelte
+++ frontend/src/routes/user/+page.svelte
@@ -6,7 +6,11 @@
     let token
     let url='http://localhost:2000/api'
     let panelButtons=["Dashboard","Shared","Notifications","Bookmarks","Bin","Subscriptions","Help"]        
-    
+    let currentPanel=0
+    function logout(){
+        localStorage.removeItem("apex_cloud")
+        window.location="/"
+    }
     async function authenticateToken() {
             try {
                 const response = await fetch(`${url}/authenticateToken`, {
@@ -43,7 +47,7 @@
     <title>Apex Cloud | {full_name}</title>
     
     <div class="flex flex-col" style="width:100vw;height:100svh">
-        <div class="w-full bg-gray-100 flex flex-row shadow-lg justify-between">
+        <div class="w-full bg-gray-100 flex flex-row shadow-lg pt-2 justify-between">
             <div class="flex flex-row gap-4 justify-between items-center">
                 <img class="w-16 pl-3 py-1" src="/logo.png" alt="">
                 
@@ -53,27 +57,29 @@
                 </div>
             </div>
             <input placeholder="⌕ Search Files" class="w-1/2 focus:outline-none  placeholder-black text-lg my-2 pl-4 rounded-xl">
-            <button class="text-lg my-3 text-white px-4 mx-4 bg-blue-500 transform transition-transform hover:scale-105 hover:bg-blue-600 hover:scale-105 duration-100 rounded-lg">Logout</button>
+            <button on:click={logout} class="text-lg my-3 text-white px-4 mx-4 bg-blue-500 transform transition-transform hover:scale-105 hover:bg-blue-600 hover:scale-105 duration-100 rounded-lg">Logout</button>
         </div>
         <div class="flex flex-grow gap-2 justify-around p-2 flex-row bg-gray-100">
-            <div class="w-1/4 py-5 flex flex-col items-left align-left gap-2 px-3 h-full rounded-lg overflow-y-auto">
-                               
-                {#each panelButtons as button}
-                    <button class="focus:bg-blue-200 flex pl-2 py-2 flex-row items-center transform transition-all duration-100 focus:outline-none  hover:scale-105 outline-none focus:scale-105 hover:bg-white rounded-xl">
-                        <img class="" src='{button}.png' alt="">
-                        <div class="pl-3 w-full text-left text-lg">{button}</div>
-                    </button>
-                {/each}
+            <div class="w-1/4 px-3 py-5 flex flex-col items-left align-left gap-2  h-full rounded-lg overflow-y-auto">
+                    <button class="text-lg rounded-lg py-2 my-1  transition-all bg-gray-500 hover:bg-blue-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Upload File</button>
+              
+                    {#each panelButtons as button,index}
+                        <button on:click={currentPanel=index} class="{currentPanel==index?"bg-blue-200":""} focus:bg-blue-200 flex pl-2 py-2 flex-row items-center transform transition-all duration-100 focus:outline-none  hover:scale-105 outline-none focus:scale-105 hover:bg-white rounded-xl">
+                            <img class="" src='{button}.png' alt="">
+                            <div class="pl-3 w-full text-left text-lg">{button}</div>
+                        </button>
+                    {/each}
             </div>
             <div class="w-3/4 flex flex-col bg-white rounded-lg">
                 <div class="flex flex-row w-full text-xl justify-between py-3 px-2 gap-3 shadow-sm">
+                    <div class="flex flex-row gap-4">
+                        <button class="px-4 text-xl rounded-lg py-1 mr-5 hover:bg-gray-100 text-black transition-all ease-in-out duration-100">←</button>
+                        
+                    </div>
                     <div>
-                        <button class="px-4 text-xl rounded-lg py-1 mr-5 hover:bg-green-400 bg-gray-500 text-white transition-all ease-in-out duration-100">Back</button>
-                        <button class="px-4 text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-blue-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Upload File</button>
-                        <button class="px-4 text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-blue-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Create Folder</button>
+                        <button class="px-4 mr-4 text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-red-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Delete</button>
               
                     </div>
-                    <button class="px-4 text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-red-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Delete</button>
               
                 </div>
 
