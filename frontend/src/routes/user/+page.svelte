3d6fa96195fd0d91a0ef4b06c1076ce6b4f9d038
Tanishq Dhote <119649887+ptsdpatient@users.noreply.github.com>
2024-11-04 09:15:28 +0530

    table body scrollable, scroll bar styling and lighter buttons

--- frontend/src/routes/user/+page.svelte
+++ frontend/src/routes/user/+page.svelte
@@ -37,6 +37,160 @@
             bookmark:false,
             selected:false,
             moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
+        },{
+            id:3,
+            name:'File3.xlsx',
+            created_at:'6:53 PM, 3 Nov',
+            bookmark:false,
+            selected:false,
+            moved:false
         }
     ]   
     let currentPanel=0
@@ -83,8 +237,7 @@
         <div class="flex flex-grow gap-2 justify-around p-2 flex-row bg-gray-200">
             <div class="w-1/4 px-3 pb-2 flex flex-col items-left align-left gap-2  h-full rounded-lg overflow-y-auto justify-between">
                 <div class="flex flex-row gap-4 items-center">
-                    <img class="w-16 py-1" src="/logo.png" alt="">
-                    
+                    <img class="w-16 py-1" src="/logo.png" alt="">                    
                     <div class="flex flex-col items-left">
                         <div class="text-xl">{full_name}</div>
                         <div class="text-sm">{email}</div>
@@ -101,15 +254,15 @@
                 <button on:click={logout} class="text-lg py-2 text-white px-4  bg-gray-500 transform transition-transform hover:scale-105 hover:bg-red-600 hover:scale-105 duration-100 rounded-lg">Logout</button>
 
             </div>
-            <div class="w-3/4 flex p-2 m-2 flex-col bg-white rounded-lg">
+            <div class="w-3/4 flex p-2 m-2 flex-col bg-white rounded-3xl">
              
                     <div class="flex flex-row gap-2  py-3">
-                        <button class="px-4  text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-blue-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Select All</button>
+                        <button class="px-4  text-xl rounded-3xl py-1 transition-all hover:bg-gray-100 text-black hover:shadow-sm ease-in-out duration-100">Select All</button>
 
                         <input placeholder="⌕ Search Files" class="focus:outline-none bg-gray-100 flex flex-grow placeholder-black text-lg pl-4 rounded-xl">
 
                         {#each operationButtons as button}
-                            <button class="px-4  text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-blue-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">{button}</button>
+                            <button class="px-4  text-xl rounded-3xl py-1 transition-all hover:bg-gray-100 text-black hover:shadow-sm ease-in-out duration-100">{button}</button>
                         {/each}
                     </div>
                     <div class="flex flex-row w-full text-xl justify-between py-3 px-2 gap-3 shadow-sm">
@@ -128,33 +281,33 @@
                         <button class="px-4 mr-4 text-xl rounded-lg py-1  transition-all bg-gray-500 hover:bg-red-500 text-white hover:shadow-xl transform hover:scale-105 ease-in-out duration-100">Delete Folder</button>
                     </div>
 
-                    <div class="bg-white h-full">
-                        <table class="w-full border-collapse text-xl">
+                    <div class="bg-white flex flex-grow overflow-y-auto" style="height:50vh;">
+                        <table class="w-full border-collapse text-xl overflow-y-auto">
                             <thead class="text-black bg-white my-4">
                               <tr class="shadow-sm ">
                                 <!-- <td class=" py-3  text-center">#</td> -->
-                                <td class=" py-3 text-left pl-5">File Name</td>
-                                <td class=" py-3 text-left pl-5">Created At</td>
-                                <td class=" py-3 text-left pl-5">Actions</td>
+                                <td class=" py-3 text-left pl-5 w-3/5">File Name</td>
+                                <td class=" py-3 text-left pl-5 w-1/5">Created At</td>
+                                <td class=" py-3 text-left pl-5 w-1/5">Actions</td>
+                                <!-- <td class=" py-3 text-left pl-5">Actions</td> -->
                               </tr>
                             </thead>
                             <tbody class="">
                                 {#each currentFiles as files,index}
-                                    <tr on:click={()=>{return files.selected=!files.selected}} class="hover:bg-gray-100 hover:shadow-sm rounded-xl hover:cursor-pointer transition-all ease-in-out">
+                                    <tr  class="hover:bg-gray-100 hover:shadow-sm rounded-xl hover:cursor-pointer transition-all ease-in-out">
                                         <!-- <td class=" py-2  text-center">{index+1}</td> -->
-                                        <td class=" py-2 text-left pl-5"><input bind:checked={files.selected} class="accent-blue-800  transform scale-125 mx-3" type="checkbox">{files.name}</td>
-                                        <td class=" py-2 text-left pl-5">{files.created_at}</td>
+                                        <td on:click={()=>{return files.selected=!files.selected}} class=" py-2 text-left"><input bind:checked={files.selected} class="accent-blue-800 hover:cursor-pointer transform scale-125 mx-3 mr-5" type="checkbox">{files.name}</td>
+                                        <td on:click={()=>{return files.selected=!files.selected}} class=" py-2 text-left pl-5">{files.created_at}</td>
                                         <td class=" py-2 text-left pl-5">
                                             <div class="flex flex-row gap-4 text-2xl">
-                                                <button>🔖</button>
-                                                <button>➦</button>
-                                                <button>🗑️</button>
+                                                <button><img src="Bookmarks.png" alt="bookmark"></button>
+                                                <button><img src="Share.png" alt="bookmark"></button>
+                                                <button><img src="Bin.png" alt="bookmark"></button>
+
                                             </div>                                    
                                         </td>
                                     </tr>    
                                 {/each}
-                                
-
                             </tbody>
                         </table>
                     </div>
